266b0a551d52582facc8071c67e6ad188f24d5d0
Lennard <lennard@spring.nl>
2022-02-03 11:04:50 +0100

    Tests

---
+++ tests/Feature/SomeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Senna\Datatable\Tests\Feature;
+
+use Senna\Datatable\Tests\TestCase;
+
+class SomeTest extends TestCase
+{
+    /**
+     * Maybe a bit fundamental, but it's a test.
+     *
+     * @return void
+     */
+    public function testAddition()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
